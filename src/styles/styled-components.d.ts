727634ce6ecd5c52270e8beb132cb787744fc91b
antoniomrrds <arqueiroverd@gmail.com>
2022-11-07 01:50:16 -0300

    config styled-components

---
+++ src/styles/styled-components.d.ts
@@ -0,0 +1,6 @@
+import { theme } from './theme';
+type theme = typeof theme;
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
