3c8c78608af2a0298098c64611f05b18147af147
antoniomrrds <arqueiroverd@gmail.com>
2022-12-26 20:44:46 -0300

    learning data fetch (getStaticProps(using api strapi))

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,4 @@
-module.exports ={
-  {
-    "presets": ["next/babel"],
-    "plugins": [["styled-components", { "ssr": true }]]
-  }
-}
-
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [['styled-components', { ssr: true }]],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <h1>hello</h1>;
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/post/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch('http://localhost:1337/api/posts');
+  const jsonPosts = await posts.json();
+  const { data } = jsonPosts;
+
+  return data;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.id}>{post.id}</h2>
+      ))}
+    </div>
+  );
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getStaticProps: GetStaticProps = async (context) => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
