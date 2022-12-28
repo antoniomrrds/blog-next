f981da2de7fac4c5301f181d1c0d519235a63784
antoniomrrds <arqueiroverd@gmail.com>
2022-12-28 20:54:03 -0300

    organizing project folders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,19 @@
 import { GetStaticProps } from 'next';
+import { HomePage } from '../containers/HomePage';
+import { getAllPosts } from '../data/posts/get-all-posts';
 import { PostData } from '../domain/post/post';
-
-const getPosts = async (): Promise<PostData[]> => {
-  const posts = await fetch('http://localhost:1337/api/posts');
-  const jsonPosts = await posts.json();
-  const { data } = jsonPosts;
-
-  return data;
-};
 
 export type HomeProps = {
   posts: PostData[];
 };
 
 export default function Home({ posts }: HomeProps) {
-  return (
-    <div>
-      {posts.map((post) => (
-        <h2 key={post.id}>{post.id}</h2>
-      ))}
-    </div>
-  );
+  return <HomePage posts={posts} />;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getStaticProps: GetStaticProps = async (context) => {
-  const posts = await getPosts();
-
+  const posts = await getAllPosts();
   return {
     props: {
       posts,
