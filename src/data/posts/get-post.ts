d8c4fe88d6c918757216ca32b06d57f691bb2975
antoniomrrds <arqueiroverd@gmail.com>
2023-01-03 17:32:04 -0300

    converting markdown for Html with remark

--- src/data/posts/get-post.ts
+++ src/data/posts/get-post.ts
@@ -1,6 +1,7 @@
 import { POSTS_URL } from '../../config/app-config';
 import { PostData } from '../../domain/post/post';
 import { fetchJson } from '../../utils/fetch-json';
+import { markdownToHtml } from '../../utils/markdown-to-html';
 
 export const getPosts = async (
   slug: string | string[],
@@ -8,5 +9,7 @@ export const getPosts = async (
   const slugString = Array.isArray(slug) ? slug[0] : slug;
   const URL = `${POSTS_URL}?slug=${slugString}`;
   const jsonPosts = await fetchJson<PostData[]>(URL);
-  return jsonPosts;
+  const content = await markdownToHtml(jsonPosts[0].attributes.content);
+  const finalContent = { ...jsonPosts[0], content };
+  return [finalContent];
 };
