47c576e0092e9d1c8043380328ab13f687d9ae61
antoniomrrds <arqueiroverd@gmail.com>
2023-01-07 16:33:51 -0300

    configuring the test using jest (testing library and jest Styled components)

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -1,4 +1,4 @@
-import { theme } from './theme';
+import { theme } from '../src/styles/theme';
 type Theme = typeof theme;
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
